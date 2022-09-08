ec86a7c46623b2b8c7cb7fe544e98e0801ee76a9
AlbertMarkSesay01 <albertmarksesay0@gmail.com>
2022-09-08 15:35:47 -0700

    Genius

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <unistd.h>
 /**
  * main - prints exactly 'and thst piece of art is useful'
  * - Dora korpar, 2015-10-19,
@@ -8,6 +7,6 @@
  */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 return (1);
 }
