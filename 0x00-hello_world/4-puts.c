73789c748eb2758d72119acaf7fca9196948f82a
AlbertMarkSesay01 <albertmarksesay0@gmail.com>
2022-09-08 14:38:30 -0700

    hello puzzle

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: prints the description specified
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
